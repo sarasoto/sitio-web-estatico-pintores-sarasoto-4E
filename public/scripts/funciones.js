777bfeac8d846ec93722efc42f53d2288e119361
sarasoto <64808351+sarasoto@users.noreply.github.com>
2020-05-23 17:36:52 -0600

    Add files via upload
    subir

---
+++ public/scripts/funciones.js
@@ -0,0 +1,4 @@
+window.alert("¡Bienvenido a Famous Art!");
+var name = prompt("Ingresa tu nombre");
+window.alert(name +" disfruta el contenido");
+
---
+++ public/scripts/funcionesfinal.js
@@ -0,0 +1,7 @@
+window.alert("¡Bienvenido, disfruta el contenido!");
+var opcion = confirm("¿Quieres salir de esta ventana?");
+if (opcion == true) {
+    window.close()
+
+}
+
